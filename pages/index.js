3eaafe51251e11f32f759136ce5c8830294f6814
marcusdinizz <marcusdinizribeiro@gmail.com>
2022-07-12 23:59:41 -0300

    homepage e esboço secretaria

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,50 @@
-
+import 'bootstrap/dist/css/bootstrap.css';
 import Layout from '../components/Layout'
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Image from 'next/image'
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 
 export default function Main() {
     return (
-        <Layout>           
-            <div className={styles.container}>
-                <div className={styles.imgcontainer}>
-                <Link href='/'>
-                    <Image src="/img/sample1.jpg" alt="" layout='fill'/>
-                </Link>
+        <Layout> 
+            <div className={styles.container} >    
+           <Container>
+            <Row>
+                <Col>
+                <div className={styles.link}>  
+                    <Link href='/'> 
+                        <Image src="/img/modalidades/crossfit.jpg" fluid />
+                    </Link>
                 </div>
+                </Col>
+                <Col>
+                <div className={styles.link}>
+                    <Link href='/'>
+                        <Image src="/img/modalidades/natacao.jpg"  fluid />
+                    </Link>
+                    </div>
+                </Col>
+                <Col>
+                    
+                <div className={styles.link}>  
+                    <Link href='/'>
+                        <Image src="/img/modalidades/ritmos.jpg" fluid  />
+                    </Link>
+                    </div>
+                </Col>
+                </Row >
+            
+            <Row >
+                <Col > <Image src="/img/modalidades/spinning.jpg" fluid  /> </Col>
+                <Col >  </Col> 
+                <Col > <Image src="/img/modalidades/musculacao.jpg" fluid  /> </Col>  
+            </Row>
+            </Container>
             </div> 
         </Layout>
     )
