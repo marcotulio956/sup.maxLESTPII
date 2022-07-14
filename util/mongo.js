f1db845b4dd0c6cab76be6261a6e993d167485c2
Marco Túlio <marcotulio.sm956@gmail.com>
2022-07-13 22:40:32 -0300

    mongo file

--- util/mongo.js
+++ util/mongo.js
@@ -1,15 +1,10 @@
-import {MongoClient} from 'mongodb'
-
-const url = process.env.DATABASE_URL
-
-const client = new MongoClient(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DATABASE_URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 export default async function connect() {
-    await client.connect()
-    const db = client.db("supmaxgym")
+    await client.connect();
+    const db = client.db("supmaxgymDb")
 
     return {db, client}
 }
